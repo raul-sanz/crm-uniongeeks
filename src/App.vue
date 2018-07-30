27b6c76032846e2bf5da2d711cd5bf5ebaedce4b
raul sanz <estraulsanz@gmail.com>
2018-07-30 12:08:03 -0500

    inicio de el proyecto crm

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <app-toolbar/>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <app-footer/>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import AppFooter from '@/components/Footer'
+import AppToolbar from '@/components/Toolbar'
+export default {
+  name: 'App',
+  components:{ AppToolbar , AppFooter },
+  data () {
+    return {
+      
+    }
+  }
 }
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
